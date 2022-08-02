90dc5ca9f34d8a49f886cec3cc6adaa0e21cf786
Ma Agustina Benitez <magustinabenitez@gmail.com>
2022-08-01 21:38:40 -0300

    se agregan funciones

--- main.js
+++ main.js
@@ -20,22 +20,33 @@ const sueldoMinPMI = 76000;
 const sueldoMinPMI2000 = 109000;
 const sueldoMinPMI3000 = 147000;
 
-// Aportes mínimos para el ingreso en cada plan
-const apMinPMI = (sueldoMinPMI * 0.0765).toFixed(2);
-const apMinPMI2000 = (sueldoMinPMI2000 * 0.0765).toFixed(2);
-const apMinPMI3000 = (sueldoMinPMI3000 * 0.0765).toFixed(2);
-
-
 function cotizar() {
     let peopleQuantity = parseInt(prompt("Ingresá cantidad de personas"));
     let ageTit= parseInt(prompt("Ingresá edad del titular"));
-    // Inicializar variables luego del case 1
-    // let ageCony = parseInt(prompt("Ingresá edad de conviviente"));
-    // let youngQuantity = parseInt(prompt("Ingresá cuántos hijos son menores de 10 años"));
-    let contributions = parseFloat(prompt("Ingresá descuento de obra social"));
-    let aporteTotal = parseFloat(((contributions * 100) / 3) * 0.0765).toFixed(2);
-    let diferenciaTope;
     let plan;
+    // Aportes mínimos para el ingreso en cada plan
+    const apMinPMI = aportesMinimos(sueldoMinPMI);
+    const apMinPMI2000 = aportesMinimos(sueldoMinPMI2000);
+    const apMinPMI3000 = aportesMinimos(sueldoMinPMI3000);
+
+    function aportesMinimos(sueldoMin){
+        let apMin = parseInt((sueldoMin * 0.0765).toFixed(2));
+        return apMin;
+    }
+
+    function diferenciaTope(aporteRequerido, aporteTotal){
+        let resultadoDifTope = (aporteRequerido - aporteTotal).toFixed(2);
+        return resultadoDifTope;
+    }
+
+    function retornarAportes(){
+        let contributions = parseFloat(prompt("Ingresá descuento de obra social"));
+        let aportes = parseFloat(((contributions * 100) / 3) * 0.0765).toFixed(2);
+        return aportes;
+    }
+
+    let aporteTotal = retornarAportes();
+
     switch(peopleQuantity) {
         case 1:
             // Si es menor de 31 años:
@@ -44,20 +55,20 @@ function cotizar() {
                 if(aporteTotal >= apReqPMIind){
                     // Si es menor de 31 años y cumple el sueldo requerido en PMI o le alcanza el mínimo de PMI 2000 abonando dif de aportes
                     if(aporteTotal < apReqPMI2000ind){
-                        diferenciaTope = (apReqPMI2000ind - aporteTotal).toFixed(2);
-                        plan = `Plan PMI con cuota bonificada o PMI 2000 con diferencia de aportes de $${diferenciaTope}`;
+                        resultadoDifTope = diferenciaTope(apReqPMI2000ind, aporteTotal);
+                        plan = `Plan PMI con cuota bonificada o PMI 2000 con diferencia de aportes de $${resultadoDifTope}`;
                     // Si es menor de 31 años y cumple el sueldo requerido en PMI 2000 o le alcanza el mínimo de PMI 3000 abonando dif de aportes
                     }else if(aporteTotal < apReqPMI3000ind){
-                        diferenciaTope = (apReqPMI3000ind - aporteTotal).toFixed(2);
-                        plan = `Plan PMI 2000 con cuota bonificada o PMI 3000 con diferencia de aportes de $${diferenciaTope}`;
+                        resultadoDifTope = diferenciaTope(apReqPMI3000ind, aporteTotal);
+                        plan = `Plan PMI 2000 con cuota bonificada o PMI 3000 con diferencia de aportes de $${resultadoDifTope}`;
                     // Si es menor de 31 años y cumple el sueldo requerido para PMI 3000
                     }else{
                         plan = `Plan PMI 3000 con cuota bonificada`;
                     }
                 // Si es menor de 31 años y el sueldo es menor al requerido pero mayor o igual al mínimo
                 }else if(aporteTotal >= apMinPMI && aporteTotal < apReqPMIind){
-                    diferenciaTope = (apReqPMIind - aporteTotal).toFixed(2);
-                    plan = `Plan PMI con diferencia de aportes de $${diferenciaTope}`;
+                    resultadoDifTope = diferenciaTope(apReqPMIind, aporteTotal);
+                    plan = `Plan PMI con diferencia de aportes de $${resultadoDifTope}`;
                 // Si es menor de 31 años pero el sueldo es menor al mínimo
                 }else{
                     plan = `No es viable el ingreso por sueldo bajo`;
@@ -68,6 +79,9 @@ function cotizar() {
             alert(plan);
             break;
         case 2:  
+            
+            let ageCony = parseInt(prompt("Ingresá edad de conviviente"));
+            let youngQuantity = parseInt(prompt("Ingresá cuántos hijos son menores de 10 años"));
 
         case 3:
             
